abd3a839b3ebdb7c15203329fe49f0da9394130f
Tri Tran <ttran@LABA.PSWAS.LOCAL>
2023-04-26 16:09:12 -0700

    login page done

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const backgroundContainer = document.getElementById("background-container");
 const background = document.getElementById("background");
 const text = document.getElementById("text");
 const choices = document.getElementById("choices");
-const movingButtons = document.getElementsByClassName("moving-button");
+const page1Buttons = document.querySelectorAll(".page-1");
+const page2Buttons = document.querySelectorAll(".page-2");
 const bedButton = document.getElementById("bed-button");
+const chairButton = document.getElementById("chair-button");
 const bedButtonInitialLeft = bedButton.offsetLeft;
 const bedButtonInitialTop = bedButton.offsetTop;
+let newTop = 0;
+let newLeft =0;
 
 // const onMouseMove = (event) => {
 //     if (!isMouseDown) return;
@@ -22,6 +26,7 @@ const bedButtonInitialTop = bedButton.offsetTop;
 const clamp = (value, min, max) => {
     return Math.min(Math.max(value, min), max);
 };
+let maxed;
 
 /*Hàm updateImageDimensions() để cập nhật kích thước của ảnh nền sau khi nó được tải xong. 
 Hàm này sẽ được gọi khi ảnh nền hoàn tất việc tải và khi cửa sổ trình duyệt được thay đổi kích thước.*/
@@ -34,32 +39,46 @@ const updateImageDimensions = () => {
   if (containerAspectRatio > imageAspectRatio) {
     background.style.width = '100%';
     background.style.height = 'auto';
+    let maxed
   } else {
     background.style.width = 'auto';
     background.style.height = '100%';
+    maxed = "height"
   }
 };
 
 background.addEventListener("load", updateImageDimensions); //Thực hiện hàm updateImageDimensions khi hình ảnh nền được tải.
 window.addEventListener("resize", updateImageDimensions); //Thực hiện hàm updateImageDimensions khi kích thước cửa sổ trình duyệt thay đổi.
 
-const moveButtons = (deltaLeft, deltaTop) => {
-    for (let button of movingButtons) {
-      const currentButtonLeft = parseInt(button.style.left) || bedButtonInitialLeft;
-      const currentButtonTop = parseInt(button.style.top) || bedButtonInitialTop;
+// const moveButtons = (deltaLeft, deltaTop) => {
+//     for (let button of page1Buttons) {
+//       const currentButtonLeft = parseInt(button.style.left) || bedButtonInitialLeft;
+//       const currentButtonTop = parseInt(button.style.top) || bedButtonInitialTop;
   
-      if (deltaLeft !== undefined) {
-        const moveRatioX = (backgroundContainer.offsetWidth - button.offsetWidth) / (background.offsetWidth - backgroundContainer.offsetWidth);
-        const newLeft = currentButtonLeft + deltaLeft * moveRatioX;
-        button.style.left = newLeft + "px";
-      }
-      if (deltaTop !== undefined) {
-        const moveRatioY = (backgroundContainer.offsetHeight - button.offsetHeight) / (background.offsetHeight - backgroundContainer.offsetHeight);
-        const newTop = currentButtonTop + deltaTop * moveRatioY;
-        button.style.top = newTop + "px";
-      }
+//       if (deltaLeft !== undefined) {
+//         const moveRatioX = (backgroundContainer.offsetWidth - button.offsetWidth) / (background.offsetWidth - backgroundContainer.offsetWidth);
+//         const newLeft = currentButtonLeft + deltaLeft * moveRatioX;
+//         button.style.left = newLeft + "px";
+//       }
+//       if (deltaTop !== undefined) {
+//         const moveRatioY = (backgroundContainer.offsetHeight - button.offsetHeight) / (background.offsetHeight - backgroundContainer.offsetHeight);
+//         const newTop = currentButtonTop + deltaTop * moveRatioY;
+//         button.style.top = newTop + "px";
+//       }
+//     }
+//   };
+
+const moveButton = (buttonToMove, direction) =>{
+    if (direction ==="up") {
+        buttonToMove.style.top = parseInt(buttonToMove.style.top)+10+"px"
+    } else if (direction === "down"){
+        buttonToMove.style.top = parseInt(buttonToMove.style.top)-10+"px"
+    } else if (direction === "left"){
+        buttonToMove.style.left = parseInt(buttonToMove.style.left)-10+"px"
+    } else if (direction === "right"){
+        buttonToMove.style.left = parseInt(buttonToMove.style.left)+10+"px"
     }
-  };
+}
 
 const onKeyDown = (event) => {
   const step = 10; // Độ dịch chuyển khi nhấn phím mũi tên
@@ -70,12 +89,29 @@ const onKeyDown = (event) => {
 
   if (event.key === "ArrowLeft") {
     newLeft = clamp(currentLeft + step, -(background.offsetWidth - backgroundContainer.offsetWidth), 0);
+    if (currentLeft != newLeft) {
+        page1Buttons.forEach(element => {
+            moveButton(element, "right")
+        })
+    }
   } else if (event.key === "ArrowRight") {
     newLeft = clamp(currentLeft - step, -(background.offsetWidth - backgroundContainer.offsetWidth), 0);
+    if (currentLeft != newLeft) {
+        moveButton(bedButton, "left")
+       
+    }
   } else if (event.key === "ArrowUp") {
     newTop = clamp(currentTop + step, -(background.offsetHeight - backgroundContainer.offsetHeight), 0);
+    if (currentTop != newTop) {
+        moveButton(bedButton, "up")
+       
+    }
   } else if (event.key === "ArrowDown") {
     newTop = clamp(currentTop - step, -(background.offsetHeight - backgroundContainer.offsetHeight), 0);
+    if (currentTop != newTop) {
+        moveButton(bedButton, "down")
+       
+    }
   } else {
     return;
   }
@@ -88,7 +124,7 @@ const onKeyDown = (event) => {
     deltaTop = newTop - currentTop;
     background.style.top = newTop + "px";
   }
-  moveButtons(deltaLeft, deltaTop);
+//   moveButtons(deltaLeft, deltaTop);
 };
 
 document.addEventListener("keydown", onKeyDown);
@@ -97,6 +133,10 @@ document.addEventListener("keydown", onKeyDown);
 // document.addEventListener("mousemove", onMouseMove);
 // document.addEventListener("mouseup", onMouseUp);
 
+
+
+//Chức năng thay đổi ảnh nền
+//Function to change background image
 const changeBackgroundButton = document.getElementById("change-background");
 const backgrounds = [
     "https://cdnb.artstation.com/p/assets/images/images/025/513/449/large/anastasia-evans-bedroom-13-1.jpg?1586030012",
